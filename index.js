1d8d03fd9ab5c8da2562efeecf53c66d309e7f7c
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-07-02 08:21:40 +0600

    mongodb use

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// todojobmy
+// xYzx3YhaAXWN4GC8
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://todojobmy:xYzx3YhaAXWN4GC8@todoapp1.ftflh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log(collection)
+  client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('Running To Do List Server');
 });
